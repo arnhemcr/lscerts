1b8c1db61f3f4eb3248545cd039bb11080b821cb
Andrew Flint <arnhemcr@gmail.com>
2023-07-02 21:34:29 +1200

    sort certificate details by expiry date ascending

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # lscerts
 
 Lscerts lists certificates for URLs.
-It reads HTTPS URLs from standard input, one per line, 
-fetches and validates the list of X509 certificates from each URL
-then writes details of the leaf certificate to standard output.
-Errors about reading or parsing URLs and fetching or validating certificates are
-written to standard error.
+It reads HTTPS URLs from standard input, one per line.
+Then it fetches and validates the list of X509 certificates from each URL,
+and writes details of each leaf certificate
+sorted by expiry date ascending.
+Errors about reading or parsing URLs and fetching or
+validating certificates are written to standard error.
 Input lines that are blank or comment (starting '#') are ignored.
-Lscerts trusts certificates issued by the same set of certificate authorities (CAs)
-as the operating system on which it is running.
+Lscerts trusts certificates issued by the same set of
+certificate authorities as the operating system on which it runs.
 
 For example, to list certificates on a couple of URLs from a Unix-like command line:
 
@@ -17,8 +18,8 @@ For example, to list certificates on a couple of URLs from a Unix-like command l
     https://wikipedia.org/
     +
     #expires toExpiry URL serialNumber issuerCA
-    2024-02-13 33w https://example.com 16...93 CN=DigiCert TLS RSA SHA256 2020 CA1, ...
     2023-07-23 3w https://wikipedia.org/ 26...53 CN=R3, ...
+    2024-02-13 33w https://example.com 16...93 CN=DigiCert TLS RSA SHA256 2020 CA1, ...
     $
 
 Output fields are:
--- lscerts.go
+++ lscerts.go
@@ -1,25 +1,29 @@
 // Copyright 2023 Andrew Flint arnhemcr@gmail.com
 //
-// This program is free software: you can redistribute it and/or modify it under the terms of
-// the GNU General Public License as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, 
+// or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// This program is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY;  without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along with this program.
-// If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License 
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 // Lscerts lists certificates for URLs.
-// It reads HTTPS URLs from standard input, one per line,
-// fetches and validates the list of X509 certificates from each URL
-// then writes details of the leaf certificate to standard output.
-// Errors about reading or parsing URLs and fetching or validating certificates are
-// written to standard error.
+// It reads HTTPS URLs from standard input, one per line.
+// Then it fetches and validates the list of X509 certificates from each URL,
+// and writes details of each leaf certificate 
+// sorted by expiry date ascending.
+// Errors about reading or parsing URLs and fetching or 
+// validating certificates are written to standard error.
 // Input lines that are blank or comment (starting '#') are ignored.
-// Lscerts trusts certificates issued by the same set of certificate authorities (CAs)
-// as the operating system on which it is running.
+// Lscerts trusts certificates issued by the same set of 
+// certificate authorities as the operating system on which it runs.
+
 package main
 
 import (
@@ -32,12 +36,14 @@ import (
 	"net"
 	"net/url"
 	"os"
+	"sort"
+	"strings"
 	"time"
 )
 
 const exec = "lscerts"
 const comment = '#'
-const fetchTimeout = 5 // seconds
+const fetchTimeout = 5 // for certificates in seconds
 
 // GetHostPort parses str as an HTTPS URL
 // returning hostPort == "<hostName>:<portNumber>" and err == nil.
@@ -48,7 +54,8 @@ func getHostPort(str string) (hostPort string, err error) {
 	case err != nil:
 		return "", fmt.Errorf("%s %w", exec, err)
 	case url.Scheme != "https":
-		return "", errors.New(fmt.Sprintf("%s \"%s\": url scheme not https", exec, str))
+		return "", errors.New(fmt.Sprintf(
+			"%s \"%s\": url scheme not https", exec, str))
 	}
 
 	hostPort = url.Host
@@ -64,10 +71,12 @@ func getHostPort(str string) (hostPort string, err error) {
 // If failed to fetch or validate the certificates,
 // fetchCert returns cert == nil and err != nil.
 func fetchCert(hostPort string) (cert *x509.Certificate, err error) {
-	conn, err := tls.DialWithDialer(&net.Dialer{Timeout: fetchTimeout * time.Second},
+	conn, err := tls.DialWithDialer(
+		&net.Dialer{Timeout: fetchTimeout * time.Second}, 
 		"tcp", hostPort, nil)
 	if err != nil {
-		// failed to either connect to hostPort within timeout or validate certificates
+		// failed to connect to hostPort in timeout 
+		// or validate certificates
 		return nil, fmt.Errorf("%s \"%s\": %w", exec, hostPort, err)
 	}
 	defer conn.Close()
@@ -109,6 +118,7 @@ func main() {
 	flag.BoolVar(&noHeader, "n", false, "no output header")
 	flag.Parse()
 
+	details := []string{}
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -128,17 +138,24 @@ func main() {
 		}
 
 		// cert is valid X509 leaf certificate for url fetched from hostPort
-		if noHeader == false {
-			fmt.Printf("%cexpires toExpiry URL serialNumber issuerCA\n", comment)
-			noHeader = true
-		}
 		expiryTime := cert.NotAfter
-		expiryDate := expiryTime.Format(time.DateOnly)
 		toExpiry := getToExpiry(expiryTime)
-		fmt.Println(expiryDate, toExpiry, url, cert.SerialNumber, cert.Issuer)
+		fields := []string{expiryTime.Format(time.DateOnly), toExpiry, url, 
+			cert.SerialNumber.String(), cert.Issuer.String()}
+		record :=  strings.Join(fields, " ")
+		details = append(details, record)
 	}
 	err := scanner.Err()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, fmt.Errorf("%s: %w", exec, err))
+	}
+
+	if noHeader == false {
+		fmt.Printf("%cexpires toExpiry URL serialNumber issuerCA\n", 
+			comment)
+	}
+	sort.Strings(details)
+	for _, detail := range details {
+		fmt.Println(detail)
 	}
 }

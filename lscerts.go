2b202cdafb91ca5762495c191fdb1e6f5ff405b9
Andrew Flint <arnhemcr@gmail.com>
2023-07-04 17:10:42 +1200

    run gofmt -s

--- lscerts.go
+++ lscerts.go
@@ -1,27 +1,27 @@
 // Copyright 2023 Andrew Flint arnhemcr@gmail.com
 //
-// This program is free software: you can redistribute it and/or modify it 
-// under the terms of the GNU General Public License as published by the 
-// Free Software Foundation, either version 3 of the License, 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License,
 // or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful, 
-// but WITHOUT ANY WARRANTY;  without even the implied warranty of 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY;  without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License 
+// You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 // Lscerts lists certificates for URLs.
 // It reads HTTPS URLs from standard input, one per line.
 // Then it fetches and validates the list of X509 certificates from each URL,
-// and writes details of each leaf certificate 
+// and writes details of each leaf certificate
 // sorted by expiry date ascending.
-// Errors about reading or parsing URLs and fetching or 
+// Errors about reading or parsing URLs and fetching or
 // validating certificates are written to standard error.
 // Input lines that are blank or comment (starting '#') are ignored.
-// Lscerts trusts certificates issued by the same set of 
+// Lscerts trusts certificates issued by the same set of
 // certificate authorities as the operating system on which it runs.
 
 package main
@@ -72,10 +72,10 @@ func getHostPort(str string) (hostPort string, err error) {
 // fetchCert returns cert == nil and err != nil.
 func fetchCert(hostPort string) (cert *x509.Certificate, err error) {
 	conn, err := tls.DialWithDialer(
-		&net.Dialer{Timeout: fetchTimeout * time.Second}, 
+		&net.Dialer{Timeout: fetchTimeout * time.Second},
 		"tcp", hostPort, nil)
 	if err != nil {
-		// failed to connect to hostPort in timeout 
+		// failed to connect to hostPort in timeout
 		// or validate certificates
 		return nil, fmt.Errorf("%s \"%s\": %w", exec, hostPort, err)
 	}
@@ -140,9 +140,9 @@ func main() {
 		// cert is valid X509 leaf certificate for url fetched from hostPort
 		expiryTime := cert.NotAfter
 		toExpiry := getToExpiry(expiryTime)
-		fields := []string{expiryTime.Format(time.DateOnly), toExpiry, url, 
+		fields := []string{expiryTime.Format(time.DateOnly), toExpiry, url,
 			cert.SerialNumber.String(), cert.Issuer.String()}
-		record :=  strings.Join(fields, " ")
+		record := strings.Join(fields, " ")
 		details = append(details, record)
 	}
 	err := scanner.Err()
@@ -151,7 +151,7 @@ func main() {
 	}
 
 	if (noHeader == false) && (1 <= len(details)) {
-		fmt.Printf("%cexpires toExpiry URL serialNumber issuerCA\n", 
+		fmt.Printf("%cexpires toExpiry URL serialNumber issuerCA\n",
 			comment)
 	}
 	sort.Strings(details)
